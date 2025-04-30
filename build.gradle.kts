4ebcec821c02f7f534e99e4018ce4dc0450efd41
MicroEJ GitHub Delivery <delivery@microej.com>
2025-04-30 16:34:22 +0000

    Version 1.3.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@
 
 import com.microej.gradle.plugins.MicroejExtension
 
-ext.set("samplesVersion", "1.3.0")
+ext.set("samplesVersion", "1.3.1")
 
 plugins {
 	alias(libs.plugins.microej.application) apply false
