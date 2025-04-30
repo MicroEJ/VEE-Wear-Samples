005064f6c8dbd707554b8e6f2b6aa03bfdcc782b
MicroEJ GitHub Delivery <delivery@microej.com>
2025-04-30 16:34:22 +0000

    Version 1.3.0

--- wear-health-app/src/main/java/com/microej/example/wear/health/activity/widget/HealthIndicator.java
+++ wear-health-app/src/main/java/com/microej/example/wear/health/activity/widget/HealthIndicator.java
@@ -8,7 +8,6 @@ import ej.microui.display.Display;
 import ej.microui.display.GraphicsContext;
 import ej.microui.display.Painter;
 import ej.mwt.Widget;
-import ej.mwt.style.Style;
 import ej.mwt.util.Size;
 
 /**
